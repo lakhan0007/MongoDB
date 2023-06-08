1f9accd2748656ad79ea608bdc1626cf63a40d77
lakhan singh rathore <107997584+lakhan0007@users.noreply.github.com>
2023-06-08 19:58:34 +0530

    MondoDB updated

---
+++ error-playground.js
@@ -0,0 +1,20 @@
+const sum = (a, b) => {
+    if (a && b) {
+        return a + b;
+    }
+    throw new Error('Invalid arguments');
+};
+
+// console.log(sum(1));
+
+// try {
+//     console.log(sum(1));
+// }
+// catch (error) {
+//     console.log('Error occurred!!!');
+//     // console.log(Error);
+// }
+
+
+    
+console.log("This works!!!");
